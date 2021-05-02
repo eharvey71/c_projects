0f7c9894d8eb1f86eecbdea3c97c0dd43d8ddb05
Eric Harvey <planetside0@gmail.com>
2021-05-02 14:56:09 -0400

    Added Inventory functions

---
+++ text_adventure/inventory.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "object.h"
+#include "misc.h"
+
+static void moveObject(const char *noun, OBJECT *from, OBJECT *to)
+{
+    OBJECT *obj = parseObject(noun);
+    if (obj == NULL)
+    {
+        printf("I don't understand what item you mean.\n");
+    }
+    else if (from != obj->location)
+    {
+        printf("You can't.\n");
+    }
+    else if (to == NULL)
+    {
+        printf("There is nobody here to give that to.\n");
+    }
+    else 
+    {
+        obj->location = to;
+        print("OK.\n");
+    }
+}
+
+void executeGet(const char *noun)
+{
+    moveObject(noun, player->location, player);
+}
+
+void executeDrop(const char *noun)
+{
+    moveObject(noun, player, player->location);
+}
+
+void executeGive(const char *noun)
+{
+    moveObject(noun, player, personHere());
+}
+
+void executeAsk(const char *noun)
+{
+    moveObject(noun, personHere(), player);
+}
+
+void executeInventory(void)
+{
+    if (listObjectsAtLocation(player) == 0)
+    {
+        printf("You are empty-handed.\n");
+    }
+}
